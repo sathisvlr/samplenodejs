3357bd70c990220e25b933cb91e90c802293ba28
Meganathan, Sathiskumar <sathiskumar.meganathan@usbank.com>
2016-12-19 13:36:54 -0800

    Added a index.html

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
+
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
