db031a034668dce9117cc3bdf0bf5ed358189a44
Meganathan, Sathiskumar <sathiskumar.meganathan@usbank.com>
2016-12-19 15:56:41 -0800

    added new branch

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,18 @@ var appRouter = function(app) {
       res.send("Hello World");
   });
 
+  app.post("/branch", function(req, res) {
+      var branchResponse =
+        {
+        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        "data": {},
+        "contextOut": [],
+        "source": "DuckDuckGo"
+        }
+      res.send(branchResponse);
+  });
+
   app.get("/account", function(req, res) {
     var accountMock = {
         "username": "nraboy",
