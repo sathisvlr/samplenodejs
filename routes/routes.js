24d77eb272840136d3cdc8c6dc8b7aa87da2b802
Meganathan, Sathiskumar <sathiskumar.meganathan@usbank.com>
2016-12-20 14:12:26 -0800

    Added new service for branch locator

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 var appRouter = function(app) {
 
+  var https = require('https');
   app.get("/", function(req, res) {
       res.send("Hello World");
   });
@@ -15,6 +16,101 @@ var appRouter = function(app) {
         }
       res.send(branchResponse);
   });
+
+  app.post("/branchlocator", function(req, res) {
+      var branchResponse =
+        {
+        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        "data": {},
+        "contextOut": [],
+        "source": "DuckDuckGo"
+        }
+        var zip = "94587";
+      getJsonFromBranchLocator(zip, function(data){
+            if(data.GetListATMorBranchReply.BranchList.length == 0)
+            {
+                spokenMsg = "<speak>The zip code <say-as interpret-as=\"digits\">" + zip +
+                    "</say-as> does not have any nearby branches.</speak>";
+                cardMsg = "The zip code " + zip + " does not have any nearby branches.";
+
+                response.tellWithCard(spokenMsg, "Branch Locator", cardMsg);
+                return;
+            }
+
+            var branchName = data.GetListATMorBranchReply.BranchList[0].Name.replace("&", "and");
+            var distance = data.GetListATMorBranchReply.BranchList[0].Distance + " miles";
+            var streetAddress = data.GetListATMorBranchReply.BranchList[0].LocationIdentifier.Address.AddressLine1.replace("&", "and");
+            var closingTime = getBranchClosingTimeForToday(data.GetListATMorBranchReply.BranchList[0]);
+
+            spokenMsg = "<speak>The closest Branch to the <say-as interpret-as=\"digits\">" + zip +
+                    "</say-as> zip code is the " + branchName + " location. It's located " + distance +
+                    " away at " + streetAddress + ". " +
+                    "The branch closes this evening at " + closingTime + ".</speak>";
+
+            cardMsg = "The closest Branch to the " + zip + " zip code is the "
+                    + branchName + " location. It's located " + distance + " away at " + streetAddress + ". " +
+                    "The branch closes this evening at " + closingTime + ".";
+
+            //response.tellWithCard(spokenMsg, "Branch Locator", cardMsg);
+            res.send(spokenMsg);
+       });
+  });
+
+  var url = function(zip){
+    return "https://publicrestservice.usbank.com/public/ATMBranchLocatorRESTService_V_8_0/GetListATMorBranch/LocationSearch/" +
+                    "StringQuery?application=parasoft&transactionid=cb6b8ea5-3331-408c-9ab3-58e18f2e5f95&output=json&searchtype=E&" +
+                    "stringquery=" + zip + "&branchfeatures=BOP";
+};
+
+var getJsonFromBranchLocator = function (zip, callback){
+    https.get(url(zip), function(res){
+    var body = '';
+
+    res.on('data', function(data){
+      body += data;
+    });
+
+    res.on('end', function(){
+      var result = JSON.parse(body);
+      callback(result);
+    });
+
+  }).on('error', function(e){
+    console.log('Error: ' + e);
+  });
+};
+
+var getBranchClosingTimeForToday = function(branch){
+
+    var d = new Date().getDay();
+    var time = "";
+
+    if(d == 0)
+        time = branch.OperationalHours.Sunday.ClosingTime;
+    else if(d == 1)
+        time =  branch.OperationalHours.Monday.ClosingTime;
+    else if(d == 2)
+        time =  branch.OperationalHours.Tuesday.ClosingTime;
+    else if(d == 3)
+        time =  branch.OperationalHours.Wednesday.ClosingTime;
+    else if(d == 4)
+        time =  branch.OperationalHours.Thursday.ClosingTime;
+    else if(d == 5)
+        time =  branch.OperationalHours.Friday.ClosingTime;
+    else if(d == 6)
+        time =  branch.OperationalHours.Saturday.ClosingTime;
+
+    var hour = time.substr(0, 2);
+    var minutes = time.substr(3, 2);
+
+    if(hour > 12)
+        return (hour - 12) + ":" + minutes + " PM";
+    if(hour == 12)
+        return hour + ":" + minutes + "PM";
+    if(hour < 12)
+        return hour + ":" + minutes + "AM";
+};
 
   app.get("/account", function(req, res) {
     var accountMock = {
